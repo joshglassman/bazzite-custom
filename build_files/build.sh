a44422e73e214f222ee8863fd91ac88cf63ac604
Josh Glassman <joshums@gmail.com>
2025-03-26 18:22:59 -0400

    Remove some unnecessary packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,17 +11,14 @@ dnf5 -y install \
     cascadia-code-fonts \
     cockpit-machines \
     cockpit-ostree \
-    containerd.io \
     devpod \
     flatpak-builder \
-    genisoimage \
     google-droid-sans-mono-fonts \
     google-go-mono-fonts \
     ibm-plex-mono-fonts \
     incus \
     incus-agent \
     iotop \
-    kcli \
     libvirt-nss \
     intel-one-mono-fonts \
     monaspace-nerd-fonts \
@@ -29,14 +26,8 @@ dnf5 -y install \
     nicstat \
     numactl \
     osbuild-selinux \
-    podman-bootc \
-    podman-machine \
-    podman-tui \
-    podmansh \
     powerline-fonts \
     python3-ramalama \
-    qemu-img \
-    qemu-system-x86-core \
     qemu-user-binfmt \
     rocm-hip \
     rocm-opencl \
@@ -44,7 +35,6 @@ dnf5 -y install \
     sysprof \
     tiptop \
     trace-cmd \
-    umoci \
     virt-manager \
     virt-viewer \
     virt-v2v
@@ -57,20 +47,3 @@ dnf5 config-manager addrepo --id="vscode" \
     --set=enabled=0
 dnf5 -y install --enable-repo="vscode" \
     code
-
-dnf5 config-manager addrepo --id="docker-ce" \
-    --set=baseurl="https://download.docker.com/linux/fedora/$releasever/$basearch/stable" \
-    --set=gpgkey="https://download.docker.com/linux/fedora/gpg" \
-    --set=gpgcheck=1 \
-    --set=enabled=0
-dnf5 -y install --enable-repo="docker-ce" \
-    docker-ce \
-    docker-ce-cli \
-    docker-buildx-plugin \
-    docker-compose-plugin \
-
-# Enable a System Unit File
-if rpm -q docker-ce >/dev/null; then
-    systemctl enable docker.socket
-fi
-systemctl enable podman.socket

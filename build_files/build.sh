0c0f82046447cdafd3960d2bf66ae2eb6e1ac716
Josh Glassman <joshums@gmail.com>
2025-05-11 11:14:55 -0400

    Add Docker and ZSH

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,13 +10,23 @@ dnf5 -y install \
     perf \
     sysprof \
     tiptop \
-    trace-cmd
+    trace-cmd \
+    zsh
 
 # Install additional packages from alternative repos
 dnf5 config-manager addrepo --id="vscode" \
     --set=baseurl="https://packages.microsoft.com/yumrepos/vscode" \
     --set=gpgkey="https://packages.microsoft.com/keys/microsoft.asc" \
-    --set=gpgcheck=1 \
-    --set=enabled=0
+    --set=gpgcheck=1
+dnf5 config-manager setopt vscode.enabled=0
 dnf5 -y install --enable-repo="vscode" \
     code
+
+dnf5 config-manager addrepo --from-repofile="https://download.docker.com/linux/fedora/docker-ce.repo"
+dnf5 config-manager setopt docker-ce-stable.enabled=0
+dnf5 install -y --enable-repo="docker-ce-stable" \
+    containerd.io
+    docker-buildx-plugin
+    docker-ce
+    docker-ce-cli
+    docker-compose-plugin

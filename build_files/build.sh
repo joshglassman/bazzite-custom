e8643c0bc6afa1f0168cfc572ec8c495c50bfe43
Josh Glassman <joshua.p.glassman@gmail.com>
2025-06-25 22:30:08 -0400

    Improve docker setup

--- build_files/build.sh
+++ build_files/build.sh
@@ -25,6 +25,17 @@ dnf5 config-manager setopt vscode.enabled=0
 dnf5 -y install --enable-repo="vscode" \
     code
 
+# Apply IP Forwarding before installing Docker to prevent messing with LXC networking
+sysctl -p
+
+# Load iptable_nat module for docker-in-docker.
+# See:
+#   - https://github.com/ublue-os/bluefin/issues/2365
+#   - https://github.com/devcontainers/features/issues/1235
+mkdir -p /etc/modules-load.d && cat >>/etc/modules-load.d/ip_tables.conf <<EOF
+iptable_nat
+EOF
+
 dnf5 config-manager addrepo --from-repofile="https://download.docker.com/linux/fedora/docker-ce.repo"
 dnf5 config-manager setopt docker-ce-stable.enabled=0
 dnf5 install -y --enable-repo="docker-ce-stable" \
@@ -32,7 +43,8 @@ dnf5 install -y --enable-repo="docker-ce-stable" \
     docker-buildx-plugin \
     docker-ce \
     docker-ce-cli \
-    docker-compose-plugin
+    docker-compose-plugin \
+    docker-desktop
 
 systemctl enable custom-setup.service
 systemctl enable docker.service
